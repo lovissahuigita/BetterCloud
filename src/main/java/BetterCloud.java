2927f5fab99dc9854ccd189ed79a2995a0745bd2
lovissahuigita <lovissahuigita@gatech.edu>
2016-06-16 22:29:05 -0400

    transformed and almost produce

--- src/main/java/BetterCloud.java
+++ src/main/java/BetterCloud.java
@@ -1,7 +1,14 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.oracle.javafx.jmx.json.JSONReader;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,7 +18,6 @@ public class BetterCloud {
     public static final String successMsg = "successful";
 
     public static void main(String[] args) {
-        System.out.print(("hello").contains("ell"));
 
         JSONParser parser = new JSONParser();
 
@@ -21,21 +27,50 @@ public class BetterCloud {
             JSONObject jsonObject = (JSONObject) obj;
 
             String id = (String) jsonObject.get("id");
-            System.out.println("\nid:" + id);
 
-            // loop array
+            // 1. Consume
             JSONArray logs = (JSONArray) jsonObject.get("logs");
-            System.out.println("\nlogs:" + logs);
+            Map<String, List<Log>> map = new HashMap<String, List<Log>>();
+
+            Log log;
+            String msg, email, logID;
             for (int i = 0; i < logs.size(); i++) {
-                String msg = (String) ((JSONObject) logs.get(i)).get("message");
-                Log log = new Log((String) ((JSONObject) logs.get(i)).get("id"), (String) ((JSONObject) logs.get(i)).get("email"), msg, msg.contains(successMsg));
-                System.out.println(log);
+
+                // Get the values of each log from the JSON file
+                msg = (String) ((JSONObject) logs.get(i)).get("message");
+                email = (String) ((JSONObject) logs.get(i)).get("email");
+                logID = (String) ((JSONObject) logs.get(i)).get("id");
+
+                // Create a new log object
+                log = new Log(logID, email, msg, msg.contains(successMsg));
+
+                // 2. Transform
+                if (map.containsKey(email)) {
+                    map.get(email).add(log);
+                } else {
+                    List<Log> logList = new ArrayList<Log>();
+                    logList.add(log);
+                    map.put(email, logList);
+                }
             }
-//            System.out.println(logs.size());
-//            Iterator<String> iterator = logs.iterator();
-//            while (iterator.hasNext()) {
-//                System.out.println(iterator.next());
-//            }
+
+            // 3.Produce
+            JSONArray arr = new JSONArray();
+            Iterator iter = map.entrySet().iterator();
+            JSONObject o;
+            while (iter.hasNext()) {
+                Map.Entry pair = (Map.Entry) iter.next();
+                o = new JSONObject();
+                o.put("email", pair.getKey());
+                o.put("total", ((ArrayList<Log>) pair.getValue()).size());
+                arr.add(o);
+//                iter.remove(); // avoids a ConcurrentModificationException
+            }
+            for (int i = 0; i < arr.size(); i++) {
+//                System.out.println(arr.get(i));
+            }
+
+//            System.out.println(map);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
